36d512ed8c441cbd57a582c9dd5e582b044d5c52
서성준 <tkflwk123@gmail.com>
2022-11-02 01:16:08 +0900

    동작구, 강남구, 종로구 사회복지시설 데이터 시각화 -> 서브플롯 이용(3행 1열)

---
+++ GNTA_JongRo.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+import json
+
+def society_welfare(): # 종로구 사회복지시설 데이터를 정제
+
+    service_key = '744b676868746b66343063514a624f'
+    url = "http://openapi.seoul.go.kr:8088/"+service_key+"/json/fcltOpenInfo_GN/1/66/1111000000" # 총 66개 데이터
+
+    response = requests.get(url)
+
+    r_dict = json.loads(response.text) # json파일을 불러온다.
+    r_dict = r_dict['fcltOpenInfo_GN']['row'] # 필요한 행만 담음.
+
+    df = pd.DataFrame(r_dict) # 데이터프레임으로 변환
+    df.drop(['JRSD_SGG_SE','FCLT_CD','JRSD_SGG_NM','JRSD_SGG_CD'],inplace=True,axis=1) # 시각화에 쓸모 없는 데이터 삭제
+    df.columns = ['시설명','시설종류','시설종류상세','시설장명','시설주소'] # 칼럼명을 알아보기 쉽게 바꿈.
+
+    df['시설종류'] = df['시설종류'].str.replace("\(노인\)","") # 중복된 단어 제거
+    df['시설종류'] = df['시설종류'].str.replace("\(소규모\)","") # 중복된 단어 제거
+    df['시설종류'] = df['시설종류'].str.replace("\(장애인\)","") # 중복된 단어 제거
+    df['시설종류상세'] = df['시설종류상세'].str.replace('자활시설', '재활시설')  # 오타 제거
+
+    result = df['시설종류상세'].value_counts()  # 각 시설종류의 개수 리턴
+
+    return result
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import GNTA_DongJak as DJ
+import GNTA_GangNam as GN
+import GNTA_JongRo as JR
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if __name__ =='__main__':
+
+    dongjak = DJ.society_welfare() # 동작구 데이터
+    gangnam = GN.society_welfare() # 강남구 데이터
+    jongro = JR.society_welfare()  # 종로구 데이터
+
+    plt.rc('font', family='Malgun Gothic')
+    plt.style.use('ggplot')
+
+    fig = plt.figure(figsize=(30,10)) # 캔버스 크기
+    ax1 = fig.add_subplot(3,1,1) # 서브플롯1
+    ax2 = fig.add_subplot(3,1,2) # 서브플롯2
+    ax3 = fig.add_subplot(3,1,3) # 서브플롯3
+
+    for gu,axe,restrict,color in zip( [dongjak,gangnam,jongro] , [ax1,ax2,ax3] , ['동작구','강남구','종로구'], ['red','green','blue']): # 3개의 구 데이터를 순환
+        x = list(gu.index) # 해당 구 데이터 인덱스
+        y = list(gu.values) # 해당 구 데이터 값
+
+        temp = axe.bar(x,y, width=0.4, color = color, label= restrict+' 복지시설')  # 데이터 막대를 temp에 저장
+
+        for rect in temp: # 막대 위에 수치 표시를 위함
+            height = rect.get_height()
+            axe.text(rect.get_x() + rect.get_width() / 2.0, height, '%d' % height, ha='center', va='bottom', size=15)
+
+        axe.set_ylim(0,25) # y축 범위 제한
+        axe.set_title(restrict+' 복지시설 수', size=18, weight='bold') # 각 화면의 제목
+        axe.set_xticklabels(x,fontsize=9,rotation=-10) # 각 화면의 x축 레이블
+        axe.legend(fontsize=10) # 각 화면의 범례
+
+    plt.tight_layout(h_pad=3,w_pad=3)  # 세로 여백과 가로 여백 설정
+    plt.show() # 표시
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
